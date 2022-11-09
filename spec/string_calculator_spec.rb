c27161cb338b654da010f87ce4ff51ef4f3b2bc8
Octavio Lugo Camacho <octavio.lugo.makingdevs@kueski.com>
2022-11-09 08:45:56 -0600

    Add the test that returns the sum of 2 numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,4 +18,12 @@ describe StringCalculator do
         result = calculator.add("99")
         expect(result).to eq(99)
     end
+
+    it "returns the sum of 2 numbers" do
+        calculator = StringCalculator.new
+        result = calculator.add("2,3")
+        expect(result).to eq(5)
+        result = calculator.add("7,11")
+        expect(result).to eq(18)
+    end
 end
