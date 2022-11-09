abf29ec63e438f98929a1088aad7cacb2136a22e
Octavio Lugo Camacho <octavio.lugo.makingdevs@kueski.com>
2022-11-09 08:52:18 -0600

    Add the test that returns the sum of any numbers with another spliter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -34,4 +34,12 @@ describe StringCalculator do
         result = calculator.add("10,20,30")
         expect(result).to eq(60)
     end
+
+    it "returns the sum of any numbers with another spliter" do
+        calculator = StringCalculator.new
+        result = calculator.add("1\n2,3\n4,5")
+        expect(result).to eq(15)
+        result = calculator.add("10\n20\n30")
+        expect(result).to eq(60)
+    end
 end
